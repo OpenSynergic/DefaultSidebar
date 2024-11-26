8fe5d5cebdc79488e7b97c777fd297c76465aa61
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-11-26 10:14:31 +0800

    chore: change register now to register as participant

--- resources/views/sidebar/register-now.blade.php
+++ resources/views/sidebar/register-now.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar-action-card card card-compact">
         <div class="card-body">
             <a href="{{ $registrationUrl }}" rel="nofollow" class="sidebar-action-link">
-                <p class="sidebar-action-p">Register Now</p>
+                <p class="sidebar-action-p">Register as Participant</p>
                 <svg class="sidebar-action-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"><path d="M160,136V240H313.37l-52.68-52.69a16,16,0,0,1,22.62-22.62l80,80a16,16,0,0,1,0,22.62l-80,80a16,16,0,0,1-22.62-22.62L313.37,272H160V376a56.06,56.06,0,0,0,56,56H424a56.06,56.06,0,0,0,56-56V136a56.06,56.06,0,0,0-56-56H216A56.06,56.06,0,0,0,160,136Z"></path><path d="M48,240a16,16,0,0,0,0,32H160V240Z"></path></svg>
             </a>
         </div>
